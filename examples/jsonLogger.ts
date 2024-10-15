faeba4e73758158468ea729a66fc1bf21643992f
vhlongon <vhlongon@gmail.com>
2024-10-15 20:20:20 +0200

    add new example json logger

---
+++ examples/jsonLogger.ts
@@ -0,0 +1,10 @@
+import { Logger } from 'tslog';
+import type { LogType } from './types';
+
+export function main(logType: LogType = 'json') {
+  const logger = new Logger({
+    type: logType,
+  });
+
+  logger.debug('Logging as JSON:', { fruit: '🍌' });
+}
