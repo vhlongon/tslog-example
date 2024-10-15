f7f0a1bb9dd565cfb8a79f368a9e919eca1209c0
vhlongon <vhlongon@gmail.com>
2024-10-15 20:51:10 +0200

    update saveLog example

--- examples/saveLog.ts
+++ examples/saveLog.ts
@@ -2,14 +2,20 @@ import { appendFileSync } from 'node:fs';
 import { Logger } from 'tslog';
 import type { LogType } from './types';
 
+// Attaches external Loggers, e.g. external log services, file system, database
+
 const saveLog = (logObj: Record<string, unknown>) => {
   appendFileSync('logs.txt', `${JSON.stringify(logObj)}\n\n`);
 };
 export function main(logType?: LogType) {
   const logger = new Logger({
     type: logType,
+    // attach different transports when creating a log instance
     attachedTransports: [saveLog],
   });
+
+  // or later
+  // logger.attachTransport(saveLog);
 
   logger.warn('I am a warn log with a json object:', { foo: 'bar' });
 }
